c5474d13708b27044ee761e7c06bb5d56421d48e
Rayan Khan <riyanswat@gmail.com>
2024-02-20 23:57:40 +0500

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lin#ie&v3q(dc^$78#=9jma0n8)0ovd*w1adj^zjvq-*=mzvs8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ readme.md
@@ -0,0 +1,3 @@
+## wagtail tutorial
+
+https://docs.wagtail.org/en/stable/getting_started/tutorial.html#:~:text=a%20part%20of.-,Now,-%2C%20go%20to%20your
